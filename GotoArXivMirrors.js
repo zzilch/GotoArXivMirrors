761ff9c3af2d4df5d7e97971be0eabd6648c3ee5
ZZ <vcchzy@gmail.com>
2019-08-06 14:08:00 +0800

    feat: auto append pdf extension

--- GotoArXivMirrors.js
+++ GotoArXivMirrors.js
@@ -13,19 +13,33 @@
 
 (function() {
     'use strict';
+    // useful funcitons
+    function incompletePDF(url){ return url.includes('/pdf') && !url.endsWith('pdf') }
+    function abstractURL(url){ return url.includes('/abs') }
+    
+    // available mirrors
     let mirrors = {
         'Main':'arxiv.org',
         'China':'xxx.itp.ac.cn',
         'Germany':'de.arxiv.org',
         'India':'in.arxiv.org',
     }
+    
+    // Tampermonkey menu items
     for(let mirror in mirrors){
-        console.log(mirror+':'+location.href.replace(location.hostname,mirrors[mirror]).replace('https','http'))
-        GM_registerMenuCommand(mirror, function(){
-            location.replace(
-                location.href.replace(location.hostname,mirrors[mirror])
-                .replace('https','http'))
-        });
+        let url = location.href.replace(location.hostname,mirrors[mirror]).replace('https','http')
+        url = incompletePDF(url) ? url+'.pdf' : url
+        console.log(mirror+':'+url)
+        GM_registerMenuCommand(mirror,()=>location.replace(url));
+    }
+    
+    // append the filename extension for downloading
+    if(incompletePDF(location.href)) { location.replace(location.href+'.pdf') }
+    
+    // complete pdf url with filename extension after the document is loaded
+    onload = function(){
+        if(abstractURL(location.href))
+        { document.querySelector("#abs > div.extra-services > div.full-text > ul > li:nth-child(1) > a").href += '.pdf' }
     }
 })();
 
