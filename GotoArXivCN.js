9ff470628207014b7190daf80da5185557f232e9
zzilch <zz@zilch.zone>
2019-08-09 15:48:32 +0800

    feat: add a script only for arXivCN

--- GotoArXivCN.js
+++ GotoArXivCN.js
@@ -1,14 +1,13 @@
 // ==UserScript==
-// @name         GotoArXivMirrors
+// @name         GotoArXivCN
 // @namespace    https://github.com/zzilch/GotoArXivMirrors
 // @homepage     https://github.com/zzilch/GotoArXivMirrors
 // @version      1.1
 // @author       zzilch
-// @description  Open arXiv sites with arXiv mirrors
+// @description  Open arXiv sites with arXivCN
 // @include      /^https?://(.*\.)?arxiv\.org/.*/
 // @include      http://xxx.itp.ac.cn/*
 // @run-at       document-start
-// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 (function() {
@@ -16,7 +15,7 @@
     // useful funcitons
     function incompletePDF(url){ return url.includes('/pdf') && !url.endsWith('pdf') }
     function abstractURL(url){ return url.includes('/abs') }
-    
+
     // available mirrors
     let mirrors = {
         'Main':'arxiv.org',
@@ -24,18 +23,17 @@
         'Germany':'de.arxiv.org',
         'India':'in.arxiv.org',
     }
-    
-    // Tampermonkey menu items
-    for(let mirror in mirrors){
-        let url = location.href.replace(location.hostname,mirrors[mirror]).replace('https','http')
+
+    // auto redirect to ArxivCN
+    if(!location.href.includes(mirrors.China)){
+        let url = location.href.replace(location.hostname,mirrors.China).replace('https','http')
         url = incompletePDF(url) ? url+'.pdf' : url
-        console.log(mirror+':'+url)
-        GM_registerMenuCommand(mirror,()=>location.replace(url));
+        location.replace(url)
     }
-    
+
     // append the filename extension for downloading
     if(incompletePDF(location.href)) { history.pushState(null,"",location.href+'.pdf') }
-    
+
     // complete pdf url with filename extension after the document is loaded
     onload = function(){
         if(abstractURL(location.href))
